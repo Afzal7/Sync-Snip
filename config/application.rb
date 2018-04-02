357649b61ca05f878bb2ebb85d49574cde93ac53
manish rai <manish.rai@w3villa.com>
2018-04-02 13:18:12 +0530

    added secret key

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module SyncrowRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
-
+    config.secret_key_base = '4f64aea9ecb58305ce2c4eea6a2cf410235e6ba4644bc6299d45fdaa8fc7570fed9def59fe241e65ca010b64fdda9c8e4ca4871288f9d6d7471955b13f87d1c2'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
